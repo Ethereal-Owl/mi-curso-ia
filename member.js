c0c57ee903617639a3e82d9a18629368ac8a2f96
ethereal_owl <npedrerom@gmail.com>
2026-03-28 16:10:20 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {   
+    return {
+        name: 'skills-member',
+        template: `
+            <div class="skills-member">
+                <h3>{{ member.name }}</h3>
+                <p>{{ member.description }}</p>
+            </div>
+        `,
+        props: {
+            member: Object
+        }
+    }       
+}
